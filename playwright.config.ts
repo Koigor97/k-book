ee6c3a896951eb847cbc844dc6bf7f9086b0bdcc
koigor97 <turaysamuel101@yahoo.com>
2025-10-25 15:02:56 +0200

    modify file

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
+	testDir: 'e2e',
+	testMatch: /.*\.test\.ts/,
 	webServer: {
-		command: 'npm run build && npm run preview',
-		port: 4173
+		command: 'pnpm --filter web build && pnpm --filter web preview -- --port 4173',
+		port: 4173,
+		reuseExistingServer: !process.env.CI
 	},
-	testDir: 'e2e'
+	use: {
+    baseURL: 'http://localhost:4173'
+  }
 });
