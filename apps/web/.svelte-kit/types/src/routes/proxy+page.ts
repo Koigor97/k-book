859b21d85fc314f0023766ee99e37e6e1caf6f4e
koigor97 <turaysamuel101@yahoo.com>
2025-10-27 19:49:08 +0200

    Untrack file

--- apps/web/.svelte-kit/types/src/routes/proxy+page.ts
+++
@@ -1,9 +0,0 @@
-// @ts-nocheck
-import type { PageLoad } from './$types';
-import type { HealthCheckResult } from '@kbook/shared';
-
-export const load = async ({ fetch }: Parameters<PageLoad>[0]) => {
-	const response = await fetch('/api/health');
-	const health = (await response.json()) as HealthCheckResult;
-	return { health };
-};
